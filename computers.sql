b8ed8f55af1152a60bd302b5986ce908b18a21e8
David Fisher <tibbon@gmail.com>
2014-04-28 14:29:13 -0400

    Lab ready

---
+++ computers.sql
@@ -0,0 +1,29 @@
+-- In case there is already a computers_lab database, drop it
+
+
+
+-- Create a computers_lab database
+
+
+
+-- Connect to the computers_demo databases
+
+
+
+-- Create a computers_models table
+-- The table should have make, model, cpu_speed, memory_size,
+--  price, release_date, photo_url, storage_amount, number_usb_ports,
+--  number_firewire_ports, number_thunderbolt_ports
+
+
+
+-- Alter the computers_models, removing the storage_size column
+-- and add storage_type and storage_size columns
+
+
+
+-- Insert 10 computers into the computers_models table
+
+
+
+-- Select all entries from the computers_models table
---
+++ televisions.sql
@@ -0,0 +1,22 @@
+-- In case there is already a televisions_lab database, drop it
+
+
+-- Create a televisions_lab database
+
+
+-- Connect to the televisions_lab databases
+
+
+-- Create a tv_models table
+--  The table should have make, model, screen_size, resolution,
+--  price, release_date, photo_url
+
+
+-- Alter the tv_models, removing the resolution column
+--  and add vertical_resolution and horizontal_resolution columns
+
+
+-- Insert 10 televisions into the tv_models table
+
+
+-- Select all entries from the tv_models table
